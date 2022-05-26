467c099f33ccdf4adb7db478a8a0032333e6ccaf
Emma Grannis <grannis.emma@gmail.com>
2022-05-26 17:47:53 -0600

    Pass props down to panel in dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
-import Loading from "./loading";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
   state = {
@@ -10,10 +34,20 @@ class Dashboard extends Component {
 
   render() {
     const dashboardClasses = classnames("dashboard");
+
     if (this.state.loading) {
       return <Loading />;
     }
-    return <main className={dashboardClasses} />;
+
+    const panels = data.map((panel) => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
