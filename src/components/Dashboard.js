06ba2739104d3a437e11f4780817d69799dca0b3
Emma Grannis <grannis.emma@gmail.com>
2022-05-26 17:29:40 -0600

    Create loading.js file, that is conditionally rendered in dashboard.js if state.loading is true

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./loading";
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
-
+    if (this.state.loading) {
+      return <Loading />;
+    }
     return <main className={dashboardClasses} />;
   }
 }
